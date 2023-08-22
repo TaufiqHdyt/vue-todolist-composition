b75a0f25629ed75f08e5a23b0deca7b25e17351e
TaufiqHdyt <TaufiqHdyt@users.noreply.github.com>
2023-08-22 12:46:49 +0000

    deploy: 9fd88c5496cec2d34b787af6463d07d20eb1352d

---
+++ assets/AuthenticatedView-c9bbb877.js
@@ -0,0 +1,1 @@
+import{q as t,o as a,c as s,a as r,t as o,u as n}from"./index-14175f2c.js";const p={__name:"AuthenticatedView",setup(c){const e=t();return(u,l)=>(a(),s("div",null,[r("h1",null,o(n(e).params.id||"User")+" Detail",1)]))}};export{p as default};
