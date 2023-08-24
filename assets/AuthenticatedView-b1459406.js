d58645bd5624bc33e535bbe6bc39d980efcc8b21
TaufiqHdyt <TaufiqHdyt@users.noreply.github.com>
2023-08-24 04:43:19 +0000

    deploy: 7c9f11d45edcb3e719d0c7e1ea09930cd8916a55

--- assets/AuthenticatedView-b1459406.js
+++
@@ -1,1 +0,0 @@
-import{q as t,o as a,c as s,a as r,t as o,u as n}from"./index-1f5d0bf2.js";const p={__name:"AuthenticatedView",setup(c){const e=t();return(u,l)=>(a(),s("div",null,[r("h1",null,o(n(e).params.id||"User")+" Detail",1)]))}};export{p as default};
--- assets/NotFoundView-b3eb4380.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c}from"./index-1f5d0bf2.js";const n={};function t(r,s){return o(),c("h1",null,"404 Not Found")}const a=e(n,[["render",t]]);export{a as default};
