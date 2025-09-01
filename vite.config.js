aebc9bdab0f8e4da175db662a00cc91dd13c6bf0
fiq <me@taufiqhdyt.com>
2025-09-01 07:55:57 +0700

    feat: add vite vue devtools

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
   // base: '/vue-todolist-composition/',
   resolve: {
     alias: {
